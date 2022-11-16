ab749ec6b0cec6dafbdc9ac8cf84f71d0fcdc386
Sophie Kowalski <skowalski57@gmail.com>
2022-11-16 19:08:59 +0100

    Display static basic notes list

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,8 +1,6 @@
 export interface Todo {
   id: number;
+  title: string;
   content: string;
-}
-
-export interface Meta {
-  totalCount: number;
+  done: boolean;
 }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,47 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
+  <q-page class="row justify-evenly q-mt-md">
+    <!-- <example-component
       title="Example component"
       active
       :todos="todos"
       :meta="meta"
-    ></example-component>
+    ></example-component> -->
+    <div class="q-pa-md" style="max-width: 600px">
+      <div v-for="todo in todos" :key="todo.id" class="q-mb-md">
+        <div class="row justify-between items-center bg-grey-1 q-py-xs q-px-md">
+          <span class="text-subtitle1">{{ todo.title }}</span>
+          <q-checkbox v-model="todo.done" class="text-right" />
+        </div>
+        <div class="text-caption q-py-xs q-px-md">{{ todo.content }}</div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import { Todo } from "components/models";
+// import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from "vue";
 
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    title: "Title 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atquesuscipit in aperiam veniam.",
+    done: false,
   },
   {
     id: 2,
-    content: 'ct2'
+    title: "Title 2",
+    content: "Reprehenderit mollitia, illo officia facere tenetur est repellat",
+    done: true,
   },
   {
     id: 3,
-    content: 'ct3'
+    title: "Title 3",
+    content: "Repellendus soluta at nemo minus, molestiae doloremque.",
+    done: false,
   },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
 ]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
 </script>
