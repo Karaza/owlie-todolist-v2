a8bbe858d1e3ab68cd32fc91e4fb39fa27463067
Sophie Kowalski <skowalski57@gmail.com>
2022-11-16 19:04:34 +0100

    Add basic login form in left menu drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -22,9 +22,46 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-      </q-list>
+      <div class="q-pa-md">
+        <q-title class="text-h6">Me connecter</q-title>
+        <p class="text-caption text-grey">
+          Entrez vos identifiants pour vous connecter
+        </p>
+        <q-form>
+          <q-input
+            v-model="email"
+            class="q-mb-md"
+            filled
+            type="email"
+            placeholder="Email"
+          />
+          <q-input
+            v-model="password"
+            class="q-mb-md"
+            filled
+            placeholder="Mot de passe"
+            :type="isPwd ? 'password' : 'text'"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+          <div>
+            <q-btn label="Me connecter" type="submit" color="secondary" /><br />
+            <q-btn
+              label="Créer un compte"
+              type="reset"
+              color="primary"
+              flat
+              class="q-mt-sm"
+            />
+          </div>
+        </q-form>
+      </div>
     </q-drawer>
 
     <q-page-container>
@@ -37,6 +74,9 @@
 import { ref } from "vue";
 
 const leftDrawerOpen = ref(false);
+const email = ref("");
+const password = ref("");
+const isPwd = ref(true);
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
