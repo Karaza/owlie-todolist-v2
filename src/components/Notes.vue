60eb2612262fd6faf52872d62c0b5a3741e48a85
Sophie Kowalski <skowalski57@gmail.com>
2022-11-20 14:28:27 +0100

    Add fields title + transition

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -3,62 +3,73 @@
     Aucun élement pour le moment...
   </p>
   <div v-else class="q-pa-md col-md-8 col">
-    <div v-for="(note, index) in notes" :key="index" class="q-mb-md">
-      <div class="q-mb-md">
-        <div
-          @click="note.expanded = !note.expanded"
-          class="row justify-between items-center bg-grey-2 q-py-xs q-px-md"
-        >
-          <span class="text-subtitle1">{{ note.title }}</span>
-          <q-checkbox
-            @click="notesStore.deleteNote(note.id as string)"
-            v-model="note.done"
-            class="text-right"
-          />
-        </div>
-        <div
-          class="cursor-edit text-caption q-py-xs q-px-md"
-          :class="{ ellipsis: !note.expanded }"
-        >
-          {{ note.content }}
-          <q-popup-edit
-            v-model="note.content"
-            :validate="(val: string) => val.length > 0"
-            v-slot="scope"
+    <transition-group
+      appear
+      enter-active-class="animated fadeIn slow"
+      leave-active-class="animated fadeOut slow"
+    >
+      <div v-for="(note, index) in notes" :key="index" class="q-mb-md">
+        <div class="q-mb-md">
+          <div
+            @click="note.expanded = !note.expanded"
+            title="Cliquez pour dérouler le contenu"
+            class="row justify-between items-center bg-grey-2 q-py-xs q-px-md cursor-pointer"
           >
-            <q-input v-model="scope.value" dense autofocus>
-              <template v-slot:after>
-                <q-btn
-                  flat
-                  dense
-                  color="negative"
-                  icon="cancel"
-                  @click.stop.prevent="scope.cancel"
-                  :rules="[
+            <span class="text-subtitle1">{{ note.title }}</span>
+            <q-checkbox
+              cursor-trash
+              @click="notesStore.deleteNote(note.id as string)"
+              v-model="note.done"
+              class="text-right"
+              color="secondary"
+              title="Supprimer la note"
+            />
+          </div>
+          <div
+            class="cursor-edit text-caption q-py-xs q-px-md"
+            :class="{ ellipsis: !note.expanded }"
+            title="Cliquez pour éditer"
+          >
+            {{ note.content }}
+            <q-popup-edit
+              v-model="note.content"
+              :validate="(val: string) => val.length > 0"
+              v-slot="scope"
+            >
+              <q-input v-model="scope.value" dense autofocus>
+                <template v-slot:after>
+                  <q-btn
+                    flat
+                    dense
+                    color="negative"
+                    icon="cancel"
+                    @click.stop.prevent="scope.cancel"
+                    :rules="[
                     (val: string) =>
                       scope.validate(val) || 'More than 5 chars required',
                   ]"
-                />
+                  />
 
-                <q-btn
-                  flat
-                  dense
-                  color="positive"
-                  icon="check_circle"
-                  @click.stop.prevent="
-                    editNote(note.id as string, scope.value, scope)
-                  "
-                  :disable="
-                    scope.validate(scope.value) === false ||
-                    scope.initialValue === scope.value
-                  "
-                />
-              </template>
-            </q-input>
-          </q-popup-edit>
+                  <q-btn
+                    flat
+                    dense
+                    color="positive"
+                    icon="check_circle"
+                    @click.stop.prevent="
+                      editNote(note.id as string, scope.value, scope)
+                    "
+                    :disable="
+                      scope.validate(scope.value) === false ||
+                      scope.initialValue === scope.value
+                    "
+                  />
+                </template>
+              </q-input>
+            </q-popup-edit>
+          </div>
         </div>
       </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
